7a1231956d3c971ed7e6035fdf7cf5afa2ab1172
Daniel <danilacasito8@gmail.com>
2026-05-22 12:38:17 +0200

    Add Ping response!

---
+++ src/main/java/com/kiosko/server/listener/ServerListener.java
@@ -0,0 +1,14 @@
+package com.kiosko.server.listener;
+
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+class ServerListener {
+    @RabbitListener(queuesToDeclare = @Queue(name = "server.connection", durable = "false"))
+    public String verificarConexión(String message) {
+        return "pong";
+    }
+
+}
