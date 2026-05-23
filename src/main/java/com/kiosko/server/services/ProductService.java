38d0c3af3efc537d94568e7d9c94890382ffe98b
Daniel <danilacasito8@gmail.com>
2026-05-23 10:40:51 +0200

    Add ability to get product by its DTO

--- src/main/java/com/kiosko/server/services/ProductService.java
+++ src/main/java/com/kiosko/server/services/ProductService.java
@@ -1,9 +1,23 @@
 package com.kiosko.server.services;
 
+import com.kiosko.server.dto.ProductDTO;
+import com.kiosko.server.models.Product;
+import com.kiosko.server.repository.ProductRepository;
+import com.kiosko.server.exceptions.ProductNotFound;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-@Service
-class ProductService {
+import java.util.Optional;
 
+@Service
+@RequiredArgsConstructor
+class ProductService {
+    private final ProductRepository productRepository;
+
+    public Product fromDto(ProductDTO productDTO) throws ProductNotFound {
+        Optional<Product> productOptional = productRepository.findById(productDTO.getId());
+        if (productOptional.isEmpty()) throw new ProductNotFound("Product with id " + productDTO.getId() + " not found");
+        return productOptional.get();
+    }
     // TODO: Make Product Search by barcode function
 }
