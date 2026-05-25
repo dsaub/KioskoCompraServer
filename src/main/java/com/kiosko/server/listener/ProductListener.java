fc65822b5516635f60f0eca03c2e94ba89ac0ac1
Daniel <danilacasito8@gmail.com>
2026-05-25 13:48:43 +0200

    try-catchc createProduct

--- src/main/java/com/kiosko/server/listener/ProductListener.java
+++ src/main/java/com/kiosko/server/listener/ProductListener.java
@@ -5,6 +5,8 @@ import com.kiosko.server.dto.ProductDTO;
 import com.kiosko.server.dto.ProductPageResponse;
 import com.kiosko.server.services.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
@@ -15,12 +17,16 @@ import tools.jackson.databind.ObjectMapper;
 public class ProductListener {
     private final ObjectMapper objectMapper = new ObjectMapper();
     private final ProductService productService;
-
+    private Logger logger = LoggerFactory.getLogger(ProductListener.class);
     @RabbitListener(queuesToDeclare = @Queue(name = "product.create", durable = "true"))
     public String createProduct(String rec) {
+        try {
+            ProductDTO productDTO = productService.store(objectMapper.readValue(rec, ProductDTO.class));
+            return objectMapper.writeValueAsString(productDTO);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
 
-        ProductDTO productDTO = productService.store(objectMapper.readValue(rec, ProductDTO.class));
-        return objectMapper.writeValueAsString(productDTO);
     }
 
     @RabbitListener(queuesToDeclare = @Queue(name = "product.search", durable = "true"))
