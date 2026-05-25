7a60cbe7b7dc59a076aaf28d342dfc837925b557
Daniel <danilacasito8@gmail.com>
2026-05-25 13:54:10 +0200

    Add NoArgsConstructor and AllArgsConstructor to ProductDTO.java

--- src/main/java/com/kiosko/server/dto/ProductDTO.java
+++ src/main/java/com/kiosko/server/dto/ProductDTO.java
@@ -1,9 +1,13 @@
 package com.kiosko.server.dto;
 
 import com.kiosko.server.models.Product;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductDTO {
     private Long id;
     private String name;
